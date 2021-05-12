dc8143296ee81c598eb636d29e51e80ea72711d6
Chris Wasser <cawasser@mac.com>
2021-05-12 13:46:42 -0400

    - baseline commit

---
+++ src/cljs/simple_grid/core.cljs
@@ -0,0 +1,11 @@
+(ns simple-grid.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rd]
+            [re-frame.core :as rf]
+            [taoensso.timbre :as log]))
+
+
+
+(defn -main []
+
+  (log/info "Running!"))
