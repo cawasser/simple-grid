0e0e684bc48119c917523f2e6acba4c791ff8e79
Chris Wasser <cawasser@mac.com>
2021-05-15 20:31:38 -0400

    - I think I have it! Splitting "layout" from "widgets" and de-ref'ing them separately seesm to do the trick(s)

--- src/cljs/simple_grid/core.cljs
+++ src/cljs/simple_grid/core.cljs
@@ -3,12 +3,19 @@
             [reagent.dom :as rd]
             [re-frame.core :as rf]
             [taoensso.timbre :as log]
-            [day8.re-frame.tracing :refer-macros [fn-traced defn-traced]]))
+            [day8.re-frame.tracing :refer-macros [fn-traced defn-traced]]
+            ["react-grid-layout" :refer (Responsive) :as ReactGridLayout]))
+
+;; region ; DEFAULTS
 
 (def default-db {:widgets {"one" {:data "one" :local 0}}
                  :timers  {"one" 0}
-                 :layout  [{:id "one" :grid-data {:x 0 :y 0 :w 2 :h 2}}]})
+                 :layout  {"one" {:i "one" :x 0 :y 0 :w 2 :h 2}}})
 
+(def default-widget
+  {:x 0 :y 0 :w 2 :h 2})
+
+;; endregion
 
 ;; region ; Subscriptions
 
@@ -22,6 +29,20 @@
   :widgets
   (fn [db _]
     (:widgets db)))
+
+
+(rf/reg-sub
+  :layout
+  (fn [db _]
+    (vals (:layout db))))
+
+
+
+(rf/reg-sub
+  :widget
+  (fn [db [_ widget-id]]
+    (get-in db [:widgets widget-id])))
+
 
 
 (rf/reg-sub
@@ -50,12 +71,17 @@
   :add-widget
   (fn-traced [db [_ name data]]
     (-> db
-      (assoc-in [:widgets name :data] data)
-      (assoc-in [:widgets name :local] 0)
-      (#(if (contains? (:timers db) data)
+      (#(if (contains? (:layout db) name)
           %
-          (assoc-in % [:timers data] 0))))))
-
+          (-> %
+            (assoc-in [:layout] (assoc (:layout db)
+                                  name
+                                  (assoc default-widget :i name)))
+            (assoc-in [:widgets name :data] data)
+            (assoc-in [:widgets name :local] 0)
+            ((fn [x] (if (contains? (:timers db) data)
+                       x
+                       (assoc-in x [:timers data] 0))))))))))
 
 
 (rf/reg-event-db
@@ -87,60 +113,165 @@
 (rf/reg-event-db
   :delete-widget
   (fn-traced [db [_ widget-id]]
-    (assoc db :widgets (dissoc (:widgets db) widget-id))))
+    (-> db
+      (assoc :widgets (dissoc (:widgets db) widget-id))
+      (assoc :layout (remove #(= widget-id (:i %)) (:layout db))))))
+
+
+(rf/reg-event-db
+  :reset-local
+  (fn-traced [db [_ widget-id]]
+    (assoc-in db [:widgets widget-id :local] 0)))
+
+
+(defn- update-layout [db {:keys [i] :as new-layout}]
+  (assoc-in db [:layout i] new-layout))
+
+(rf/reg-event-db
+  :update-layout
+  (fn-traced [db [_ new-layout]]
+    (let [cooked (map #(zipmap '(:i :x :y :w :h) %)
+                   (map (juxt :i :x :y :w :h) new-layout))]
+      (assoc db :layout (zipmap (map :i cooked) cooked)))))
 
 ;; endregion
+
+(comment
+  (def db @re-frame.db/app-db)
+  (def i "one")
+  (def new-layout [{:i "one" :x 0 :y 0 :w 2 :h 2}
+                   {:i "two" :x 0 :y 0 :w 2 :h 2}])
+
+  (map (fn [x] (:i x)) new-layout)
+  (zipmap (map :i new-layout) new-layout)
+
+  (assoc db :layout (zipmap (map :i new-layout) new-layout))
+
+  (map #(zipmap '(:i :x :y :w :h) %)
+    (map (juxt :i :x :y :w :h) new-layout))
+  ())
 
 
 ;; region ; Widgets
 
 (defn- make-content [name sub data]
-  (str sub " :: " @data))
+  [:p (str sub " :: " @data)])
 
 
 (defn- title-bar [name]
-  [:div.title-bar {:on-click #(rf/dispatch [:delete-widget name])}
-   [:h3 name]])
+  [:div.widget-banner.title-wrapper.grid-toolbar.move-cursor
+   [:h3 {:style {:color :black}} name]
+   [:button.button {:on-click #(rf/dispatch [:delete-widget name])} "Delete"]])
 
 
 (defn- base [name content]
-  [:div.widget.parent {:key name}
+  [:div.widget-parent
    (title-bar name)
    [:div.widget.widget-content content]])
 
 
-(defn- widget [name w]
-  (log/info "widget" w)
-
+(defn- widget [name]
   (log/info "widget OUTER" name)
 
-  (let [data (rf/subscribe [:timer (:data w)])
-        local (rf/subscribe [:local name])]
+  (rf/dispatch-sync [:reset-local name])
+
+  (let [widget @(rf/subscribe [:widget name])
+        data   (rf/subscribe [:timer (:data widget)])
+        local  (rf/subscribe [:local name])]
     (fn []
       (log/info "widget INNER" name)
       (base
         name
         [:div
-         [:button.button {:on-click #(rf/dispatch [:reset (:data w)])} "reset"]
+         [:button.button {:on-click #(rf/dispatch [:reset (:data widget)])} "reset"]
          [:button.button {:on-click #(rf/dispatch [:local name])} "local"]
          [:p (str "local: " @local)]
-         [:p (make-content name (:data w) data)]]))))
+         (make-content name (:data widget) data)]))))
 
 ;; endregion
+
 
 ;; region ; Grid
 
 (defn- simple-grid []
   (log/info "simple-grid OUTER")
-  (let [widgets (rf/subscribe [:widgets])]
+
+  (let [widgets (rf/subscribe [:layout])]
     (fn []
       (log/info "simple-grid INNER")
-      [:div.grid
+      [:div.grid {:style {}}
        (doall
-         (for [[k w] @widgets]
-           ^{:key k} [widget k w]))])))
+         (for [{:keys [i]} @widgets]
+           ^{:key i} [widget i]))])))
+
+
+(def dummy-layout (r/atom [{:i "one" :x 0 :y 0 :w 2 :h 2}
+                           {:i "two" :x 0 :y 0 :w 2 :h 2}
+                           {:i "three" :x 0 :y 0 :w 2 :h 2}]))
+(def dummy-widgets (r/atom [[:div.widget {:key "one" :style {:color :white}} "a"]
+                            [:div.widget {:key "two" :style {:color :white}} "b"]
+                            [:div.widget {:key "three" :style {:color :white}} "c"]]))
+(def dummy-reactive-widgets (r/atom [[:div.widget {:key "one"} [(fn [] [:div {:style {:color :white}} "a"])]]
+                                     [:div.widget {:key "two"} [(fn [] [:div {:style {:color :white}} "b"])]]
+                                     [:div.widget {:key "three"} [(fn [] [:div {:style {:color :white}} "c"])]]]))
+
+
+(defn- simple-responsive-grid []
+  (log/info "responsive-grid OUTER")
+
+  (let [layout (rf/subscribe [:layout])]
+    (fn []
+      (log/info "responsive-grid INNER")
+      [:div.grid-container
+       (into [:> ReactGridLayout
+              {:id         "dashboard-widget-grid"
+               :class      "layout"
+               :layout     @dummy-layout
+               :cols       12                               ;{:lg 12 :md 10 :sm 6 :xs 4 :xxs 2};12
+               :width      1536
+               :row-height 50
+               :style      {:height 500}
+               ;:breakpoints {:lg 2048 :md 1024 :sm 768 :xs 480 :xxs 0}
+               :on-change  #()
+               :item-props {:class "widget-component"}}]
+         @dummy-reactive-widgets)])))
+
+
+(defn on-layout-change [new]
+  (let [chg (js->clj new :keywordize-keys true)
+        fst (first chg)]
+    (if (and
+          (not (empty? chg))
+          (<= 1 (count chg))
+          (not= (:i fst) "null"))
+      (do
+        (rf/dispatch [:update-layout chg])))))
+
+
+(defn- responsive-grid []
+  (log/info "responsive-grid OUTER")
+
+  (let [layout (rf/subscribe [:layout])]
+    (fn []
+      (log/info "responsive-grid INNER")
+      [:div.grid-container
+       (into [:> ReactGridLayout
+              {:id             "dashboard-widget-grid"
+               :class          "layout"
+               :layout         @layout
+               :cols           12
+               :width          1536
+               :row-height     50
+               :style          {:height 500}
+               :onLayoutChange #(on-layout-change %)
+               :item-props     {:class "widget-component"}}]
+
+         (doall
+           (for [{:keys [i]} @layout]
+             ^{:key i} [:div.widget {:key i} [widget i]])))])))
 
 ;; endregion
+
 
 ;; region ; main-page
 
@@ -155,6 +286,7 @@
 
 
 (defn- main-page []
+  (log/info "main-page")
   [:div
    [:h3 "Simple Grid"]
    [:button.button {:on-click #(rf/dispatch-sync [:tick])} "Tick!"]
@@ -162,7 +294,9 @@
    [:button.button {:on-click #(rf/dispatch [:add-widget "two" "two"])} "Add \"two\""]
    [:button.button {:on-click #(rf/dispatch [:add-widget "three" "one"])} "Add \"three\""]
    [timers]
-   [simple-grid]])
+   ;[simple-grid]
+   ;[simple-responsive-grid]
+   [responsive-grid]])
 
 ;; endregion
 
@@ -209,4 +343,22 @@
 
   (rf/dispatch [:delete-widget "two"])
 
+  (contains? (:layout db) "one")
+
+
+  ())
+
+
+(comment
+  (def db @re-frame.db/app-db)
+  (def old-layout (:layout db))
+  (def widget-id "one")
+  (remove #(= widget-id (:id %)) (:layout db))
+
+
+  (def layout (rf/subscribe [:layout]))
+  (into []
+    (for [{:keys [i]} @layout]
+      ^{:key i} [:div.widget {:key i}
+                 [widget i]]))
   ())
